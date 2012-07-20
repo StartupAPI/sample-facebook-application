bef17dc1be582d665e1e0e065da686554902b0ca
Sergey Chernyshev <github.com@antispam.sergeychernyshev.com>
2012-07-20 17:00:06 -0400

    Added link to Github project Made button call conditional upon the Facebook module configuration.

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ $facebook_module = AuthenticationModule::get('facebook');
 // We'll be fetching friends data into this array
 $friends = array();
 
-if (!is_null($user)) {
+if (!is_null($facebook_module) && !is_null($user)) {
 	// You can work with users, but it's recommended to tie your data to accounts, not users
 	#$current_account = Account::getCurrentAccount($user);
 
@@ -59,6 +59,11 @@ if (!is_null($user)) {
 if (!is_null($user)) {
 ?>
 <h1>Friends of <?php echo $user->getName() ?>!</h1>
+<div style="width: 400px; max-width: 100%">
+<a href="https://github.com/StartupAPI/sample-facebook-application/" target="_blank" style="float: left"><img alt="Octocat.png" src="http://startupapi.org/w/images/thumb/6/61/Octocat.png/50px-Octocat.png" width="50" height="50" border="0" align="top" style="margin-right: 1em"></a>
+This is a sample Facebook application powered by <a href="http://www.startupapi.com">Startup API</a>, you can see the <a href="https://github.com/StartupAPI/sample-facebook-application/" target="_blank">code on Github</a>.
+</div>
+<div style="clear: both"></div>
 <div class="fb-like" data-href="https://github.com/StartupAPI/sample-facebook-application" data-send="true" data-show-faces="false"></div>
 <div>
 <?php
@@ -80,7 +85,9 @@ else
 ?>
 <h1>Sample Facebook Application</h1>
 <?php
-	?><p><?php $facebook_module->renderRegistrationForm(); ?></p><?php
+	if (!is_null($facebook_module)) {
+		?><p><?php $facebook_module->renderRegistrationForm(); ?></p><?php
+	}
 }
 ?> 
 </body>
